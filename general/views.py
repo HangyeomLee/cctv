6b383a532edeaa6edf97e54dcf5bf12a1a24e82d
parkseonggyu <psg9806@naver.com>
2024-06-03 17:10:52 +0900

    http되는카메라

--- general/views.py
+++ general/views.py
@@ -110,6 +110,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
 
+
 class FetchAndSaveImageView(APIView):
     def get(self, request):
         try:
